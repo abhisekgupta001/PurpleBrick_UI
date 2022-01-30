5ff59b92f7139a09ab80c72bb02919450bed059c
Abhisek Gupta <guptaabhi6488@gmail.com>
2022-01-30 21:06:34 +0530

    refactor: made the folder structure

--- public/main.js
+++ public/main.js
@@ -1,6 +1,14 @@
 const docSideBar = document.querySelector(".doc__sidebar");
 const hamburger = document.querySelector(".pb-hamburger");
+const docMain = document.querySelector(".doc__main");
 
 hamburger.addEventListener("click", ()=>{
+    docSideBar.classList.remove("hide-doc__sidebar");
     docSideBar.classList.toggle("show-doc__sidebar");
 })
+
+docMain.addEventListener("click", ()=>{
+    docSideBar.classList.add("hide-doc__sidebar");
+    docSideBar.classList.remove("show-doc__sidebar");
+
+});
