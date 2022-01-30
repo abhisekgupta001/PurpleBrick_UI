9fd37400248c8c549ec121373ab8e2b273967dcd
Abhisek Gupta <guptaabhi6488@gmail.com>
2022-01-30 15:05:02 +0530

    feat: added sidebar and nav for docs.

--- public/main.js
+++ public/main.js
@@ -1,1 +1,6 @@
-console.log("main js working");
+const docSideBar = document.querySelector(".doc__sidebar");
+const hamburger = document.querySelector(".pb-hamburger");
+
+hamburger.addEventListener("click", ()=>{
+    docSideBar.classList.toggle("show-doc__sidebar");
+})
