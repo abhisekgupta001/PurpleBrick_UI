16fb3979aeaeddb643b4dfe3e6a65f801bcf59b9
Abhisek Gupta <guptaabhi6488@gmail.com>
2022-01-31 23:43:26 +0530

    feat: added badge component

---
+++ public/components/badge/badge.js
@@ -0,0 +1,7 @@
+const buttonToggle = document.querySelector(".button__toggle");
+
+
+buttonToggle.addEventListener("click", ()=>{
+const bagdeToggle = document.querySelector(".badge__button--toggle");
+    bagdeToggle.classList.toggle("hide-badge");
+})
